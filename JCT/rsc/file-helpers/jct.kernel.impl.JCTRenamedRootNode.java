dca4ff197268070cba6e42da46b54ccddab3992d
Jovan El Hoayek <jhoayek@gmail.com>
2017-10-01 16:03:50 -0400

    Removed everything except SAD.

--- JCT/rsc/file-helpers/jct.kernel.impl.JCTRenamedRootNode.java
+++
@@ -1,5 +0,0 @@
-private void readObject(final java.io.ObjectInputStream in) throws IOException, ClassNotFoundException
-{
-    in.defaultReadObject();
-    this.factory = new SoftReference<IJCTFactory>(null);
-}
--- PADL Serialiser NeoDatis/doc/run-tutorial.sh
+++
@@ -1,2 +0,0 @@
-java -classpath ./tutorial.jar:./../neodatis-odb.jar org.neodatis.odb.tutorial.Tutorial1
-
